5bf37f1868ad5b5af7ff6e7b8157f778d4306abd
enspdf <sebastianhc1995@hotmail.com>
2015-10-15 17:21:06 -0500

    script sql alumnos

---
+++ Crud-Alumnos/FrmAlumnos.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Crud_Alumnos
+{
+    partial class FrmAlumnos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigoAlumno = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgDatosAlumnos = new System.Windows.Forms.DataGridView();
+            this.txtNombreAlumno = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellidoAlumno = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDireccionAlumno = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtValorMatricula = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.CodigoAlumno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValorMatricula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreApellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatosAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código Alumno:";
+            // 
+            // txtCodigoAlumno
+            // 
+            this.txtCodigoAlumno.Location = new System.Drawing.Point(163, 20);
+            this.txtCodigoAlumno.Name = "txtCodigoAlumno";
+            this.txtCodigoAlumno.Size = new System.Drawing.Size(343, 20);
+            this.txtCodigoAlumno.TabIndex = 1;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(148, 207);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // dgDatosAlumnos
+            // 
+            this.dgDatosAlumnos.AllowUserToAddRows = false;
+            this.dgDatosAlumnos.AllowUserToDeleteRows = false;
+            this.dgDatosAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDatosAlumnos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CodigoAlumno,
+            this.ValorMatricula,
+            this.NombreApellido,
+            this.Direccion});
+            this.dgDatosAlumnos.Location = new System.Drawing.Point(19, 246);
+            this.dgDatosAlumnos.Name = "dgDatosAlumnos";
+            this.dgDatosAlumnos.ReadOnly = true;
+            this.dgDatosAlumnos.Size = new System.Drawing.Size(487, 249);
+            this.dgDatosAlumnos.TabIndex = 3;
+            // 
+            // txtNombreAlumno
+            // 
+            this.txtNombreAlumno.Location = new System.Drawing.Point(163, 59);
+            this.txtNombreAlumno.Name = "txtNombreAlumno";
+            this.txtNombreAlumno.Size = new System.Drawing.Size(343, 20);
+            this.txtNombreAlumno.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre:";
+            // 
+            // txtApellidoAlumno
+            // 
+            this.txtApellidoAlumno.Location = new System.Drawing.Point(163, 97);
+            this.txtApellidoAlumno.Name = "txtApellidoAlumno";
+            this.txtApellidoAlumno.Size = new System.Drawing.Size(343, 20);
+            this.txtApellidoAlumno.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Apellido:";
+            // 
+            // txtDireccionAlumno
+            // 
+            this.txtDireccionAlumno.Location = new System.Drawing.Point(163, 133);
+            this.txtDireccionAlumno.Name = "txtDireccionAlumno";
+            this.txtDireccionAlumno.Size = new System.Drawing.Size(343, 20);
+            this.txtDireccionAlumno.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Dirección:";
+            // 
+            // txtValorMatricula
+            // 
+            this.txtValorMatricula.Location = new System.Drawing.Point(163, 171);
+            this.txtValorMatricula.Name = "txtValorMatricula";
+            this.txtValorMatricula.Size = new System.Drawing.Size(343, 20);
+            this.txtValorMatricula.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Valor Matricula:";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(229, 207);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 12;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(310, 207);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 13;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // CodigoAlumno
+            // 
+            this.CodigoAlumno.HeaderText = "Código Alumno";
+            this.CodigoAlumno.Name = "CodigoAlumno";
+            this.CodigoAlumno.ReadOnly = true;
+            this.CodigoAlumno.Width = 110;
+            // 
+            // ValorMatricula
+            // 
+            this.ValorMatricula.HeaderText = "Valor Matricula";
+            this.ValorMatricula.Name = "ValorMatricula";
+            this.ValorMatricula.ReadOnly = true;
+            this.ValorMatricula.Width = 110;
+            // 
+            // NombreApellido
+            // 
+            this.NombreApellido.HeaderText = "Nombre y Apellido";
+            this.NombreApellido.Name = "NombreApellido";
+            this.NombreApellido.ReadOnly = true;
+            this.NombreApellido.Width = 120;
+            // 
+            // Direccion
+            // 
+            this.Direccion.HeaderText = "Dirección";
+            this.Direccion.Name = "Direccion";
+            this.Direccion.ReadOnly = true;
+            // 
+            // FrmAlumnos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 509);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.txtValorMatricula);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDireccionAlumno);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtApellidoAlumno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombreAlumno);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgDatosAlumnos);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.txtCodigoAlumno);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmAlumnos";
+            this.Text = "Alumnos";
+            this.Load += new System.EventHandler(this.FrmAlumnos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatosAlumnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigoAlumno;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgDatosAlumnos;
+        private System.Windows.Forms.TextBox txtNombreAlumno;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellidoAlumno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDireccionAlumno;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtValorMatricula;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodigoAlumno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValorMatricula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreApellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
+    }
+}
+
---
+++ Crud-Alumnos/FrmAlumnos.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud_Alumnos
+{
+    public partial class FrmAlumnos : Form
+    {
+        public FrmAlumnos()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            dgDatosAlumnos.AutoGenerateColumns = false;
+        }
+
+        private void CargarDatos()
+        {
+            using (var context = new EntidadesAlumnos())
+            {
+                dgDatosAlumnos.DataSource = (from q in context.Alumno
+                                             from w in context.Matricula
+                                             select new { q, w }).ToList();
+                dgDatosAlumnos.Refresh();
+            }
+        }
+
+        private void Limpiar()
+        {
+            txtApellidoAlumno.Text = string.Empty;
+            txtCodigoAlumno.Text = string.Empty;
+            txtDireccionAlumno.Text = string.Empty;
+            txtNombreAlumno.Text = string.Empty;
+            txtValorMatricula.Text = string.Empty;
+        }
+
+        private void FrmAlumnos_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (var context = new EntidadesAlumnos())
+            {
+                Alumno oAlumno = new Alumno()
+                {
+                    CodAlumno = Convert.ToInt32(txtCodigoAlumno.Text),
+                    Nombre = txtNombreAlumno.Text,
+                    Apellido = txtApellidoAlumno.Text,
+                    Direccion = txtDireccionAlumno.Text
+                };
+
+                Matricula oMatricula = new Matricula()
+                {
+                    CodAlumno = oAlumno.CodAlumno,
+                    ValorMatricula = Convert.ToInt32(txtValorMatricula.Text)
+                };
+
+                context.Alumno.Add(oAlumno);
+                context.Matricula.Add(oMatricula);
+
+                context.SaveChanges();
+
+                MessageBox.Show("El estudiante " + txtNombreAlumno.Text + " " + txtApellidoAlumno + " ha sido registrado satsfactoriamente");
+
+                CargarDatos();
+                Limpiar();
+            }
+        }
+
+
+    }
+}
